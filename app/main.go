68e827dbfcc2e7b4a3c94ca50e4ef1a78e352933
pogzyb <pogzyb@umich.edu>
2019-07-28 22:00:49 -0400

    Create simple asteroid api call

--- app/main.go
+++ app/main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func getAsteroidsToday() map[string]interface{} {
+	var data map[string]interface{}
+	nasa := "https://api.nasa.gov/neo/rest/v1/feed/today?detailed=true&api_key=%s"
+	key := os.Getenv("NASA_API_KEY")
+	uri := fmt.Sprintf(nasa, key)
+
+	resp, err := http.Get(uri)
+	if err != nil {
+		log.Fatalf("Couldn't access NASA API: %s", err.Error())
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Couldn't read response data: %s", err.Error())
+	}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		log.Fatalf("Couldn't jsonify data: %s", err.Error())
+	}
+	return data
+}
+
+func main() {
+	obj := getAsteroidsToday()
+	fmt.Println(obj)
+}
